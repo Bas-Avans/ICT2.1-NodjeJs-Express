74b935f28324965033c0e79316fcec56be7e61d2
BlasixMac <blasixcorp@gmail.com>
2025-09-04 14:54:45 +0200

    Added MySQL support

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,29 @@
-var express = require('express');
+require("dotenv").config();
+
+var express = require("express");
 var router = express.Router();
 
+const mysql = require("mysql2");
+const connection = mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+});
+
+connection.connect();
+
+connection.query("SELECT * FROM film", (err, rows, fields) => {
+  if (err) throw err;
+
+  console.log(rows[0]);
+});
+
+connection.end();
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express" });
 });
 
 module.exports = router;
