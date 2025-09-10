20dea158c836e84650c28f3f28bb8c7f900d0ec3
BlasixMac <blasixcorp@gmail.com>
2025-09-10 15:08:19 +0200

    added auth pages

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,16 +8,27 @@ router.get("/", function (req, res, next) {
   res.render("index");
 });
 
-/* GET all films */
+/* GET all films page */
 router.get("/films", filmController.getFilms);
-
-/* GET film by ID */
+/* GET film by ID page */
 router.get("/films/:id", filmController.getFilm);
 
-/* GET all actors */
+/* GET all actors page */
 router.get("/actors", actorController.getActors);
-
-/* GET actor by ID */
+/* GET actor by ID page */
 router.get("/actors/:id", actorController.getActor);
+
+/* GET login page */
+router.get("/login", function (req, res, next) {
+  res.render("auth/login");
+});
+/* GET register page */
+router.get("/register", function (req, res, next) {
+  res.render("auth/register");
+});
+/* Get account details page */
+router.get("/account", function (req, res, next) {
+  res.render("auth/account");
+});
 
 module.exports = router;
